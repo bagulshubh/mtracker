296d9c750241ff454bbb49133ba8d95636d70db6
legion <bagulshubham72@gmail.com>
2024-06-23 19:01:13 +0530

    Split feature completed

--- src/componets/pages/Scanner.jsx
+++ src/componets/pages/Scanner.jsx
@@ -1,32 +1,44 @@
-import React, { useContext } from 'react'
+import React, { useContext, useReducer, useState } from 'react'
 import UserContext from '../../context/user/UserContext'
 import { uploadScanner } from '../../services/User/AuthServices';
+import Navbar from "../../componets/Navbar";
 
 const Scanner = () => {
 
     const context = useContext(UserContext);
     const user = context.user;
+    const setUser = context.setUser;
     const token = context.token;
+    const [loading , setLoading ] = useState(false);
 
     const onChangeFile = async (event)=>{
+       
         event.stopPropagation();
         event.preventDefault();
         const selectedFile = event.target.files[0];
         //console.log(selectedFile)
         let formData = new FormData();
         formData.append("scanner", selectedFile)
+        setLoading(true)
         const tempUser = await uploadScanner(token,formData);
+        setUser(tempUser);
+        setLoading(false);
     }
 
   return (
-    <div>
+    <div className='scanner-wrapper'>
 
-        
-            <img src={user.scanner}></img> 
-            <input type='file' onChange={onChangeFile}></input>
-            <div>Upload image</div>
-        
+      <Navbar></Navbar>
 
+      {
+        loading ? <div className='loader'></div> : <div>
+          
+          <div className='scanner-con'>
+            <img src={user.scanner} className='scanner-img'></img>
+            <input onChange={onChangeFile} type='file' className='scanner-input'></input>
+          </div>
+        </div>
+      }
     </div>
   )
 }
