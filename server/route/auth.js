1bea3c83890100a09ace519467619240d8b9b51a
legion <bagulshubham72@gmail.com>
2024-09-01 19:40:59 +0530

    Redis Fixes

--- server/route/auth.js
+++ server/route/auth.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const {signup , login  } = require("../controller/Auth")
 const {uploadScanner ,getUser} = require("../controller/User")
 const {auth}  = require("../middleware/authMiddleware");
-const { userCache } = require("../middleware/redisMiddleware");
 
 router.post("/signup",signup);
 router.post("/login",login);
