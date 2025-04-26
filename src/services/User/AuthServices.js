a2124ce015ed2f891a2f03a872401975ce9545fd
legion <bagulshubham72@gmail.com>
2025-04-26 18:11:48 +0530

    New server url

--- src/services/User/AuthServices.js
+++ src/services/User/AuthServices.js
@@ -1,4 +1,4 @@
-const BASE_URL =    "https://mtracker-0sct.onrender.com" //   "http://localhost:4000"  
+const BASE_URL =   "https://mtracker-g4mv.onrender.com"
 
 export const signUp = async (data)=>{
     try{
